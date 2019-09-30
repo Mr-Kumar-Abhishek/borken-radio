a06b7ada515904b4700ac714199639ad82fc047f
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-09-30 09:16:05 +0000

    get now playing data from the server

--- public/client.js
+++ public/client.js
@@ -1,2 +1,10 @@
 // client-side js
 // run by the browser each time your view template is loaded
+
+function get_data() {
+     $.getJSON("https://broken.radio:444/api/nowplaying",function(data) {
+        console.log(data);
+     });
+}
+
+$(get_data());
