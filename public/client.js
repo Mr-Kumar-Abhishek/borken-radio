dca869177c69820895822cc8b5907d90987d442b
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-09-30 09:39:10 +0000

    📠📠 Checkpoint ./public/client.js:197979/63

--- public/client.js
+++ public/client.js
@@ -1,9 +1,14 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
+function set_menus(menu_data){
+  
+}
+
 function get_data() {
      $.getJSON("https://broken.radio:444/api/nowplaying",function(data) {
         console.log(data);
+        set_menus(data)
      });
 }
 
