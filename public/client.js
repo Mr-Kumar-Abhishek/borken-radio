eca8eb7987d376093cb15be25591c3bdd1337171
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-09-30 09:59:31 +0000

    log short codes of all stations

--- public/client.js
+++ public/client.js
@@ -2,13 +2,15 @@
 // run by the browser each time your view template is loaded
 
 function set_menus(menu_data){
-  
+  for(var i = 0; i < menu_data.length; i++){
+    console.log(menu_data[i].station.shortcode);
+  }
 }
 
 function get_data() {
      $.getJSON("https://broken.radio:444/api/nowplaying",function(data) {
-        console.log(data);
-        set_menus(data)
+        // console.log(data);
+        set_menus(data);
      });
 }
 
