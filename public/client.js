764dba1d4ed20e11bc5acb2ed78cac79d505ccfa
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-09-30 10:18:36 +0000

    log now playing data

--- public/client.js
+++ public/client.js
@@ -4,14 +4,14 @@
 function set_menus(menu_data){
   for(var i = 0; i < menu_data.length; i++){
     var station_code = menu_data[i].station.shortcode;
-    console.log(station_code);
+    // console.log(station_code);
     $("#station-menu").append('<li class="nav-item"><a class="nav-link" href="#">'+ station_code +'</a></li>');
   }
 }
 
 function get_data() {
      $.getJSON("https://broken.radio:444/api/nowplaying",function(data) {
-        // console.log(data);
+        console.log(data);
         set_menus(data);
      });
 }
