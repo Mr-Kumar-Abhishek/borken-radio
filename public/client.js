c098a2bd413e887191787b75b909855d107a473d
Abhishek Kumar <mr.kumar.abhishek@email.com>
2019-09-30 10:12:55 +0000

    include station code in nav bar from api

--- public/client.js
+++ public/client.js
@@ -3,7 +3,9 @@
 
 function set_menus(menu_data){
   for(var i = 0; i < menu_data.length; i++){
-    console.log(menu_data[i].station.shortcode);
+    var station_code = menu_data[i].station.shortcode;
+    console.log(station_code);
+    $("#station-menu").append('<li class="nav-item"><a class="nav-link" href="#">'+ station_code +'</a></li>');
   }
 }
 
